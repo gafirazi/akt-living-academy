8cbb1d67febe0902f3607c82b5a143bbc5dd1bce
gafiirfandi <gafiirfandi45@gmail.com>
2023-09-25 20:36:28 +0700

    remove sqlite from settings py

--- aktlivingacademy/settings.py
+++ aktlivingacademy/settings.py
@@ -97,12 +97,12 @@ CORS_ALLOW_HEADERS = [
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 PRODUCTION = os.environ.get('DATABASE_URL') is not None
 if PRODUCTION:
